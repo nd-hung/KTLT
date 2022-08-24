f1849d352b80ac8cad23a3870b65d7a48ce25626
nd-hung <hungdhnt@gmail.com>
2022-08-24 11:16:50 +0700

    Added recommended solution

---
+++ KTLT-He2022/Dethi/De1/Cau1/main.cpp
@@ -0,0 +1,14 @@
+// Created by Hung on 24/08/2022.
+#include<stdio.h>
+#define Square(x) x*x
+int main()
+{
+    int a=6, b=2;
+    printf("%d", Square(a + b));
+    return 0;
+}
+
+/*
+ * Kết quả khi chạy chương trình là 20.
+ * Giải thích: Từ định nghĩa macro, Square(6+2) = Square(6+2*6+2) = 20.
+ */
