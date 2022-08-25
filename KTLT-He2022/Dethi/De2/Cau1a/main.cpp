bfc18de835762bb7a89c92c9e3859815232404f1
nd-hung <hungdhnt@gmail.com>
2022-08-25 15:03:49 +0700

    Added recommended solution

--- KTLT-He2022/Dethi/De2/Cau1a/main.cpp
+++ KTLT-He2022/Dethi/De2/Cau1a/main.cpp
@@ -1,13 +1,14 @@
 // Created by Hung on 24/08/2022.
 #include<stdio.h>
+
 #define Sum(x, y) x+y
-int main()
-{
-    int a=5,b=10,c=15,d=20;
-    printf("%d", Sum(a*b, c+d));
+
+int main() {
+    int a = 5, b = 10, c = 15, d = 20;
+    printf("%d", Sum(a * b, c + d));
     return 0;
 }
 /*
-Ket qua = 85
-Giai thich, theo dinh nghia macro, Sum(5*10, 15+20) = 5*10+15+20 = 85
+Kết quả = 85
+Giải thích: Theo định nghĩa macro ở trên, Sum(5*10, 15+20) = 5*10+15+20 = 85
  */
